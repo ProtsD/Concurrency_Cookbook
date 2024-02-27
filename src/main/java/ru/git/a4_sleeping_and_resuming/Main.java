ed59dd285a8c968f76c702c057c5bf0a311171ad
RUPROTSD <protsdv@gmail.com>
2024-02-27 12:25:04 +0300

    Chapter 1. Sections from 1 to 5.

---
+++ src/main/java/ru/git/a4_sleeping_and_resuming/Main.java
@@ -0,0 +1,17 @@
+package ru.git.a4_sleeping_and_resuming;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleClock clock = new ConsoleClock();
+        Thread thread = new Thread(clock);
+        thread.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/git/a5_waiting_for_the_finalization/Main.java
@@ -0,0 +1,24 @@
+package ru.git.a5_waiting_for_the_finalization;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        DataSourcesLoader dsLoader = new DataSourcesLoader();
+        Thread thread1 = new Thread(dsLoader, "DataSourceThread");
+        NetworkConnectionsLoader ncLoader = new NetworkConnectionsLoader();
+        Thread thread2 = new Thread(ncLoader, "NetworkConnectionLoader");
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join(2000);
+//            thread1.join();
+            System.out.println("//////////////////" + thread1.getState());
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("Main: Configuration has been loaded: %s\n", new Date());
+
+    }
+}
