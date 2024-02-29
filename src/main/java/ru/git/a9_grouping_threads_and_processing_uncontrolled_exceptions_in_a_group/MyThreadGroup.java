1d0c4eeef294a0d482c4534b43835d0b65375831
RUPROTSD <protsdv@gmail.com>
2024-02-29 22:08:03 +0300

    Chapter 1. Sections 9 and 10.

---
+++ src/main/java/ru/git/a9_grouping_threads_and_processing_uncontrolled_exceptions_in_a_group/MyThreadGroup.java
@@ -0,0 +1,15 @@
+package ru.git.a9_grouping_threads_and_processing_uncontrolled_exceptions_in_a_group;
+
+public class MyThreadGroup extends ThreadGroup {
+    public MyThreadGroup(String name) {
+        super(name);
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.printf("The thread %s has thrown an Exception\n", t.getId());
+        e.printStackTrace(System.out);
+        System.out.printf("Terminating the rest of the Threads\n");
+        interrupt();
+    }
+}
