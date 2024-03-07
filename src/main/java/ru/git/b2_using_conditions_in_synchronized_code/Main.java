98032a8bfe0f266d0c06530c2ffed54e6428fd77
RUPROTSD <protsdv@gmail.com>
2024-03-07 23:53:49 +0300

    Chapter 2. Sections 1 and 4.

---
+++ src/main/java/ru/git/b2_using_conditions_in_synchronized_code/Main.java
@@ -0,0 +1,14 @@
+package ru.git.b2_using_conditions_in_synchronized_code;
+
+public class Main {
+    public static void main(String[] args) {
+        EventStorage storage = new EventStorage();
+        Producer producer = new Producer(storage);
+        Thread thread1 = new Thread(producer);
+        Consumer consumer = new Consumer(storage);
+        Thread thread2 = new Thread(consumer);
+        thread2.start();
+        thread1.start();
+    }
+
+}
---
+++ src/main/java/ru/git/b2_using_conditions_in_synchronized_code/Producer.java
@@ -0,0 +1,16 @@
+package ru.git.b2_using_conditions_in_synchronized_code;
+
+public class Producer implements Runnable {
+    private EventStorage storage;
+
+    public Producer(EventStorage storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            storage.set();
+        }
+    }
+}
