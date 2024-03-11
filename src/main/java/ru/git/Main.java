57daf69e32c86a3c0ad8b2c9daedf287c2ec8600
RUPROTSD <protsdv@gmail.com>
2024-03-11 22:18:38 +0300

    Chapter 3. Sections from 1 to 7.

---
+++ src/main/java/ru/git/Main.java
@@ -0,0 +1,8 @@
+package ru.git;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "Completing and linking tasks asynchronously";
+        System.out.println(s.toLowerCase().replace(" ", "_"));
+    }
+}
